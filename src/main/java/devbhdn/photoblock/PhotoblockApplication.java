e6bae3fa35a04ce6331e10b4d982012397777ceb
Bohdan Hovorukha <hovorukha.bohdan@gmail.com>
2024-12-16 18:31:37 +0200

    Added infrastructure

---
+++ src/main/java/devbhdn/photoblock/PhotoblockApplication.java
@@ -0,0 +1,13 @@
+package devbhdn.photoblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoblockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoblockApplication.class, args);
+    }
+
+}
