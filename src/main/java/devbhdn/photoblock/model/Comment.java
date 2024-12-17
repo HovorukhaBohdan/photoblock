51fb77a583f2eb843a9472fcc0b7c1fc202a9808
Bohdan Hovorukha <hovorukha.bohdan@gmail.com>
2024-12-17 20:18:51 +0200

    Fixed checkstyle

--- src/main/java/devbhdn/photoblock/model/Comment.java
+++ src/main/java/devbhdn/photoblock/model/Comment.java
@@ -1,13 +1,19 @@
 package devbhdn.photoblock.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
-
-import java.time.LocalDateTime;
 
 @Data
 @Entity
